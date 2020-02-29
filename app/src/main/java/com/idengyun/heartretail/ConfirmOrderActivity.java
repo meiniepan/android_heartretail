59cecf4d6ea9e2894353293a03022855867ec641
wangboning <meiniepan@163.com>
2020-02-29 11:23:22 +0800

    create confirmOrderActivity

---
+++ app/src/main/java/com/idengyun/heartretail/ConfirmOrderActivity.java
@@ -0,0 +1,22 @@
+package com.idengyun.heartretail;
+
+import android.os.Bundle;
+
+import com.dengyun.baselibrary.base.activity.BaseActivity;
+
+/**
+ * @author Burning
+ * @description:
+ * @date :2020-02-29 11:16
+ */
+public class ConfirmOrderActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+
+    }
+}
