4d7b9e42126a07ea23b7671518ad8644624b8342
aLang <lichunjiang@idengyun.com>
2020-03-02 15:36:40 +0800

    加入 眼睛 图标

--- app/src/main/java/com/idengyun/heartretail/login/LoginFragment.java
+++ app/src/main/java/com/idengyun/heartretail/login/LoginFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
@@ -17,16 +20,31 @@ import com.idengyun.heartretail.R;
  *
  * @author aLang
  */
-public final class LoginFragment extends BaseFragment implements RadioGroup.OnCheckedChangeListener {
+public final class LoginFragment extends BaseFragment implements RadioGroup.OnCheckedChangeListener, View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
+    /* 标签指示器相关 */
     private RadioGroup radio_group;
     private TextView tv_login_indicator;
     private View view_login_indicator;
     private TextView tv_register_indicator;
     private View view_register_indicator;
+
+    /* 登录相关 */
     private View layout_login_content;
-    private View layout_register_content;
+    private EditText et_login_mobile;
+    private EditText et_login_pwd;
+    private CheckBox cb_login_eye;
+    private View tv_forget_pwd;
     private View tv_login;
+
+    /* 注册相关 */
+    private View layout_register_content;
+    private EditText et_register_mobile;
+    private EditText et_register_verify_code;
+    private View tv_verify_code;
+    private EditText et_register_pwd;
+    private EditText cb_register_eye;
+    private EditText et_invite_code;
     private View tv_register;
 
     @Override
@@ -71,18 +89,46 @@ public final class LoginFragment extends BaseFragment implements RadioGroup.OnCh
         }
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+    }
+
     private void findViewById(@NonNull View view) {
         radio_group = view.findViewById(R.id.radio_group);
         tv_login_indicator = view.findViewById(R.id.tv_login_indicator);
         view_login_indicator = view.findViewById(R.id.view_login_indicator);
         tv_register_indicator = view.findViewById(R.id.tv_register_indicator);
         view_register_indicator = view.findViewById(R.id.view_register_indicator);
+
         layout_login_content = view.findViewById(R.id.layout_login_content);
-        layout_register_content = view.findViewById(R.id.layout_register_content);
+        et_login_mobile = view.findViewById(R.id.et_login_mobile);
+        et_login_pwd = view.findViewById(R.id.et_login_pwd);
+        cb_login_eye = view.findViewById(R.id.cb_login_eye);
+        tv_forget_pwd = view.findViewById(R.id.tv_forget_pwd);
         tv_login = view.findViewById(R.id.tv_login);
+
+        layout_register_content = view.findViewById(R.id.layout_register_content);
+        et_register_mobile = view.findViewById(R.id.et_register_mobile);
+        et_register_verify_code = view.findViewById(R.id.et_register_verify_code);
+        tv_verify_code = view.findViewById(R.id.tv_verify_code);
+        et_register_pwd = view.findViewById(R.id.et_register_pwd);
+        cb_register_eye = view.findViewById(R.id.cb_register_eye);
+        et_invite_code = view.findViewById(R.id.et_invite_code);
         tv_register = view.findViewById(R.id.tv_register);
 
         radio_group.setOnCheckedChangeListener(this);
         radio_group.check(R.id.rb_login);
+
+//        et_login_mobile.addTextChangedListener(this);
+//        et_login_pwd.addTextChangedListener(this);
+        cb_login_eye.setOnCheckedChangeListener(this);
+        tv_forget_pwd.setOnClickListener(this);
+        tv_login.setOnClickListener(this);
     }
 }
