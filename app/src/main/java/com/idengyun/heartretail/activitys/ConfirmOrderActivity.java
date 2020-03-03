e1638728de2d520b9898488c3ca006a4ab8a63c9
meiniepan <meiniepan@idengyun.com>
2020-03-03 11:35:15 +0800

    替换标题栏

--- app/src/main/java/com/idengyun/heartretail/activitys/ConfirmOrderActivity.java
+++ app/src/main/java/com/idengyun/heartretail/activitys/ConfirmOrderActivity.java
@@ -1,10 +1,13 @@
 package com.idengyun.heartretail.activitys;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.dengyun.baselibrary.base.activity.BaseActivity;
 import com.dengyun.baselibrary.widgets.toolbar.BaseToolBar;
 import com.idengyun.heartretail.R;
+import com.idengyun.heartretail.shop.ShopListActivity;
 
 import butterknife.BindView;
 
@@ -15,6 +18,10 @@ import butterknife.BindView;
  */
 public class ConfirmOrderActivity extends BaseActivity {
 
+    public static void start(Context context) {
+        Intent starter = new Intent(context, ConfirmOrderActivity.class);
+        context.startActivity(starter);
+    }
     @Override
     protected int getLayoutId() {
         return R.layout.activity_confirm_order_retail;
