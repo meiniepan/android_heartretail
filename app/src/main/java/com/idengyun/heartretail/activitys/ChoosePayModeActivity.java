b40026b1373d5c3a7516a212fc9c76c918a2016d
meiniepan <meiniepan@idengyun.com>
2020-03-03 10:42:59 +0800

    新增 activity

---
+++ app/src/main/java/com/idengyun/heartretail/activitys/ChoosePayModeActivity.java
@@ -0,0 +1,23 @@
+package com.idengyun.heartretail.activitys;
+
+import android.os.Bundle;
+
+import com.dengyun.baselibrary.base.activity.BaseActivity;
+import com.idengyun.heartretail.R;
+
+/**
+ * @author Burning
+ * @description:
+ * @date :2020/3/3 0003 10:30
+ */
+public class ChoosePayModeActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_pay_mode;
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+
+    }
+}
---
+++ app/src/main/java/com/idengyun/heartretail/activitys/ConfirmOrderActivity.java
@@ -0,0 +1,26 @@
+package com.idengyun.heartretail.activitys;
+
+import android.os.Bundle;
+
+import com.dengyun.baselibrary.base.activity.BaseActivity;
+import com.dengyun.baselibrary.widgets.toolbar.BaseToolBar;
+import com.idengyun.heartretail.R;
+
+import butterknife.BindView;
+
+/**
+ * @author Burning
+ * @description:
+ * @date :2020-02-29 11:16
+ */
+public class ConfirmOrderActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_confirm_order_retail;
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+    }
+}
