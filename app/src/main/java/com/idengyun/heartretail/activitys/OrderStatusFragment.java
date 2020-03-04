c9873fc1ef18db33be5f06603043182ca0b02fa8
Burning <meiniepan@idengyun.com>
2020-03-04 14:42:12 +0800

    update 我的订单页面

---
+++ app/src/main/java/com/idengyun/heartretail/activitys/OrderStatusFragment.java
@@ -0,0 +1,26 @@
+package com.idengyun.heartretail.activitys;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.dengyun.baselibrary.base.fragment.BaseFragment;
+
+/**
+ * @author Burning
+ * @description:订单状态详情
+ * 待付款,代销中,待发货,待收货,待评价
+ * @date :2020/3/4 0004 13:25
+ */
+public class OrderStatusFragment extends BaseFragment {
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void initViews(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+    }
+}
