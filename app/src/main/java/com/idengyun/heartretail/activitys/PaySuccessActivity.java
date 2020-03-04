cdebfbf31bff6226095befcd1367fcf58989d43d
Burning <meiniepan@idengyun.com>
2020-03-04 11:59:35 +0800

    update 选择支付方式页面

--- app/src/main/java/com/idengyun/heartretail/activitys/PaySuccessActivity.java
+++ app/src/main/java/com/idengyun/heartretail/activitys/PaySuccessActivity.java
@@ -7,7 +7,7 @@ import com.idengyun.heartretail.R;
 
 /**
  * @author Burning
- * @description:
+ * @description:支付完成
  * @date :2020/3/3 0003 10:31
  */
 public class PaySuccessActivity extends BaseActivity {
