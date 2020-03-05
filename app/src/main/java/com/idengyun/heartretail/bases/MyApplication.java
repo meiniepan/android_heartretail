4bb97fa53e7c2e15df7c1807de2abc8aaa98476a
zhoubo <zhoubo@idengyun.com>
2020-03-05 11:01:58 +0800

    添加、配置友盟统计、添加、配置神策埋点、添加、配置升级功能

--- app/src/main/java/com/idengyun/heartretail/bases/MyApplication.java
+++ app/src/main/java/com/idengyun/heartretail/bases/MyApplication.java
@@ -37,11 +37,16 @@ import com.scwang.smartrefresh.layout.api.RefreshHeader;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
 import com.scwang.smartrefresh.layout.header.ClassicsHeader;
+import com.sensorsdata.analytics.android.sdk.SAConfigOptions;
+import com.sensorsdata.analytics.android.sdk.SensorsAnalyticsAutoTrackEventType;
+import com.sensorsdata.analytics.android.sdk.SensorsDataAPI;
+import com.sensorsdata.analytics.android.sdk.SensorsDataDynamicSuperProperties;
 import com.umeng.commonsdk.UMConfigure;
 import com.zhoubo07.bannerlib.banner.BannerConfig;
 import com.zhoubo07.bannerlib.banner.BannerDisplayImageHolder;
 import com.zhoubo07.bannerlib.banner.SimpleImageBannerBean;
 
+import org.json.JSONObject;
 import org.litepal.LitePalApplication;
 
 import java.lang.reflect.Field;
@@ -87,7 +92,7 @@ public class MyApplication extends LitePalApplication {
             initUpdateModule();
             //初始化下载模块
             initDownloadModule();
-//            initSADate();
+            initSADate();
             initShareModule();
             //Android7.0相机权限问题
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
@@ -100,12 +105,64 @@ public class MyApplication extends LitePalApplication {
         }
     }
 
+    private void initSADate() {
+        // 数据接收的 URL
+        final String SA_SERVER_URL = "https://idata-dcs.idengyun.com/sa?project=xls";
+        final String SA_SERVER_URL_TEXT = "https://idata-dcs.idengyun.com/sa?project=xlstest";
+
+        //设置 SAConfigOptions，传入数据接收地址 SA_SERVER_URL
+        SAConfigOptions saConfigOptions = new SAConfigOptions(AppConfig.isDebug ? SA_SERVER_URL_TEXT : SA_SERVER_URL);
+
+        //通过 SAConfigOptions 设置神策 SDK，每个条件都非必须，开发者可根据自己实际情况设置，更多设置可参考 SAConfigOptions 类中方法注释
+        saConfigOptions.setAutoTrackEventType(SensorsAnalyticsAutoTrackEventType.APP_CLICK | // 开启全埋点点击事件
+                SensorsAnalyticsAutoTrackEventType.APP_START |      //开启全埋点启动事件
+                SensorsAnalyticsAutoTrackEventType.APP_END |        //开启全埋点退出事件
+                SensorsAnalyticsAutoTrackEventType.APP_VIEW_SCREEN)     //开启全埋点浏览事件
+                .enableLog(AppConfig.isDebug)        //开启神策调试日志，默认关闭(调试时，可开启日志)。
+                .enableHeatMap(false)   //设置点击图是否可用
+                .enableHeatMapConfirmDialog(false) // 设置点击图提示对话框是否可用
+                .enableVisualizedAutoTrack(false) //设置可视化全埋点是否可用
+                .enableVisualizedAutoTrackConfirmDialog(false) //设置可视化全埋点提示对话框是否可用
+                .enableTrackScreenOrientation(true) //是否开启屏幕方向采集
+                .enableTrackAppCrash();     //开启 crash 采集
+        //需要在主线程初始化神策 SDK
+        SensorsDataAPI.startWithConfigOptions(this, saConfigOptions);
+        //开启fragment自动收集
+        SensorsDataAPI.sharedInstance().trackFragmentAppViewScreen();
+
+        /*市场渠道 //初始化我们SDK后 调用这段代码，用于记录安装事件、渠道追踪。*/
+        try {
+            JSONObject properties = new JSONObject();
+            //这里的 DownloadChannel 负责记录下载商店的渠道。
+            String channel = WalleChannelReader.getChannel(this.getApplicationContext());
+            properties.put("DownloadChannel", channel);
+            SensorsDataAPI.sharedInstance().trackInstallation("AppInstall", properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 初始化 SDK 后，设置动态公共属性
+        SensorsDataAPI.sharedInstance().registerDynamicSuperProperties(new SensorsDataDynamicSuperProperties() {
+            @Override
+            public JSONObject getDynamicSuperProperties() {
+                try {
+                    // 比如 isLogin() 是用于获取用户当前的登录状态，SDK 会自动获取 getDynamicSuperProperties 中的属性添加到触发的事件中。
+                    JSONObject jo = new JSONObject();
+                    jo.put("platformType", "android");
+                    return jo;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        });
+    }
+
     @Override
     public Resources getResources() {
         Resources res = super.getResources();
-        Configuration config=new Configuration();
+        Configuration config = new Configuration();
         config.setToDefaults();
-        res.updateConfiguration(config,res.getDisplayMetrics());
+        res.updateConfiguration(config, res.getDisplayMetrics());
         return res;
     }
 
@@ -209,7 +266,7 @@ public class MyApplication extends LitePalApplication {
         BannerConfig.setBannerDisplayImageHolder(new BannerDisplayImageHolder() {
             @Override
             public void disPlayImage(Context context, ImageView iv, SimpleImageBannerBean data) {
-                ImageApi.displayImage(context,iv,data.getBannerImageUrl(),data.getBannerImageDefult(),data.getBannerImageDefult());
+                ImageApi.displayImage(context, iv, data.getBannerImageUrl(), data.getBannerImageDefult(), data.getBannerImageDefult());
             }
         });
 
