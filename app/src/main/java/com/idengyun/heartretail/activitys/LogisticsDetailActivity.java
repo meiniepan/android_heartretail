44d34ef34c00f0536c376a27eaee03360489745d
Burning <meiniepan@idengyun.com>
2020-03-06 12:06:18 +0800

    update 查看快递&快递详情

---
+++ app/src/main/java/com/idengyun/heartretail/activitys/LogisticsDetailActivity.java
@@ -0,0 +1,23 @@
+package com.idengyun.heartretail.activitys;
+
+import android.os.Bundle;
+
+import com.dengyun.baselibrary.base.activity.BaseActivity;
+import com.idengyun.heartretail.R;
+
+/**
+ * @author Burning
+ * @description:
+ * @date :2020/3/6 0006 9:06
+ */
+public class LogisticsDetailActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_logistics_detail;
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+
+    }
+}
