5469d24a7a5141ac2cd75e793bfccbb75dd8f926
zhoubo <zhoubo@idengyun.com>
2020-03-09 08:55:01 +0800

    添加地图模块

---
+++ maplibrary/src/main/java/com/idengyun/maplibrary/fragment/DyMapFragment.java
@@ -0,0 +1,14 @@
+package com.idengyun.maplibrary.fragment;
+
+import com.amap.api.maps.SupportMapFragment;
+
+/**
+ * @Title 装载地图map的fragment容器
+ *
+ * @Desc: 与 MapView 一样提供对 AMap 对象（地图的控制类）的访问权
+ *      与 MapView 相比 SupportMapFragment 方便之处在于其可以更好的管理地图的生命周期，布局灵活。
+ * @Author: zhoubo
+ * @CreateDate: 2020-03-09 08:11
+ */
+public class DyMapFragment extends SupportMapFragment {
+}
