33985a59c4689400461538932659e596f3040295
Burning <meiniepan@idengyun.com>
2020-03-12 12:12:20 +0800

    update 完善确认订单流程,代销逻辑

--- app/src/main/java/com/idengyun/heartretail/beans/OrderListBean.java
+++ app/src/main/java/com/idengyun/heartretail/beans/OrderListBean.java
@@ -10,35 +10,9 @@ import java.util.List;
  * @description:
  * @date :2020/3/10 0010 15:17
  */
-public class OrderListBean implements Parcelable {
+public class OrderListBean {
     public String ordersCount;
     public List<OrderStatusBean> orders;
 
-    protected OrderListBean(Parcel in) {
-        ordersCount = in.readString();
-        orders = in.createTypedArrayList(OrderStatusBean.CREATOR);
-    }
 
-    public static final Creator<OrderListBean> CREATOR = new Creator<OrderListBean>() {
-        @Override
-        public OrderListBean createFromParcel(Parcel in) {
-            return new OrderListBean(in);
-        }
-
-        @Override
-        public OrderListBean[] newArray(int size) {
-            return new OrderListBean[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(ordersCount);
-        dest.writeTypedList(orders);
-    }
 }
