9d60e3404432435bdde436485b3118a0c3936758
zhoubo <zhoubo@idengyun.com>
2020-03-24 08:53:41 +0800

    修改网络框架的String类型问题，添加多图上传，添加身份证、银行卡多图上传

--- baselibrary/src/main/java/com/dengyun/baselibrary/net/NetApi.java
+++ baselibrary/src/main/java/com/dengyun/baselibrary/net/NetApi.java
@@ -15,6 +15,7 @@ import com.orhanobut.logger.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import io.reactivex.Observable;
 import okhttp3.Response;
@@ -37,7 +38,7 @@ public class NetApi {
      * 网络请求封装，不传泛型，回调返回Json，普通okgo形式请求
      */
     public static void getString(NetOption netOption, JsonCallback<String> jsonCallback) {
-        getData(netOption,jsonCallback);
+        getData(netOption, jsonCallback);
     }
 
     public static Response getStringSync(NetOption netOption) {
@@ -45,45 +46,47 @@ public class NetApi {
     }
 
     public static void getFormString(NetOption netOption, JsonCallback<String> jsonCallback) {
-        getFormData(netOption,jsonCallback);
+        getFormData(netOption, jsonCallback);
     }
 
-    public static Response getFormStringSync(NetOption netOption)  {
+    public static Response getFormStringSync(NetOption netOption) {
         return getFormDataSync(netOption);
     }
 
-    public static <T> void getData(NetOption netOption, JsonCallback<T> jsonCallback){
-        getData(RequestMethod.POST_JSON,netOption,jsonCallback);
+    public static <T> void getData(NetOption netOption, JsonCallback<T> jsonCallback) {
+        getData(RequestMethod.POST_JSON, netOption, jsonCallback);
     }
-    public static  Response getDataSync(NetOption netOption)  {
-        return getDataSync(RequestMethod.POST_JSON,netOption);
+
+    public static Response getDataSync(NetOption netOption) {
+        return getDataSync(RequestMethod.POST_JSON, netOption);
     }
 
     /**
      * 网络请求封装，传泛型类，回调返回泛型类普通okgo形式请求
      */
     public static <T> void getFormData(NetOption netOption, JsonCallback<T> jsonCallback) {
-        getData(RequestMethod.POST_FORM,netOption,jsonCallback);
+        getData(RequestMethod.POST_FORM, netOption, jsonCallback);
     }
 
     public static <T> Response getFormDataSync(NetOption netOption) {
-        return getDataSync(RequestMethod.POST_FORM,netOption);
+        return getDataSync(RequestMethod.POST_FORM, netOption);
     }
 
     /**
      * 网络请求封装，传泛型类，回调返回泛型类普通okgo形式请求
+     *
      * @param requestMethod 请求方式
      * @param netOption     配置参数类
      * @param jsonCallback  回调
      * @param <T>
      */
-    public static <T> void getData(@RequestMethod int requestMethod,NetOption netOption, JsonCallback<T> jsonCallback) {
+    public static <T> void getData(@RequestMethod int requestMethod, NetOption netOption, JsonCallback<T> jsonCallback) {
         if (!NetworkUtils.isConnected()) {
             jsonCallback.onNoNet();
             return;
         }
 
-        if(requestMethod == RequestMethod.GET){
+        if (requestMethod == RequestMethod.GET) {
             DealParamsUtil.dealUrlForGet(netOption);
             pringLog(netOption.getUrl());
 
@@ -91,18 +94,18 @@ public class NetApi {
                     .tag(getRequestTag(netOption))
                     .headers(netOption.getHeaders())
                     .execute(jsonCallback);
-        }else if(requestMethod == RequestMethod.POST_JSON){
+        } else if (requestMethod == RequestMethod.POST_JSON) {
             String jsonParams = DealParamsUtil.getDealParams(netOption);
-            pringLog(netOption.getUrl(),jsonParams);
+            pringLog(netOption.getUrl(), jsonParams);
             OkGo.<T>post(netOption.getUrl())
                     .tag(getRequestTag(netOption))
                     .headers(netOption.getHeaders())
                     .upJson(jsonParams)
                     .execute(jsonCallback);
 
-        }else if(requestMethod == RequestMethod.POST_FORM){
+        } else if (requestMethod == RequestMethod.POST_FORM) {
             String jsonParams = DealParamsUtil.getDealParams(netOption);
-            pringLog(netOption.getUrl(),jsonParams);
+            pringLog(netOption.getUrl(), jsonParams);
             HttpParams httpParams = new HttpParams();
             httpParams.put(netOption.getParams());
             OkGo.<T>post(netOption.getUrl())
@@ -115,12 +118,13 @@ public class NetApi {
 
     /**
      * 网络请求封装 同步请求
+     *
      * @param requestMethod 请求方式
      * @param netOption     配置参数类
      * @param <T>
      */
     public static <T> Response getDataSync(@RequestMethod int requestMethod, NetOption netOption) {
-        if(requestMethod == RequestMethod.GET){
+        if (requestMethod == RequestMethod.GET) {
             DealParamsUtil.dealUrlForGet(netOption);
             pringLog(netOption.getUrl());
             try {
@@ -131,9 +135,9 @@ public class NetApi {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }else if(requestMethod == RequestMethod.POST_JSON){
+        } else if (requestMethod == RequestMethod.POST_JSON) {
             String jsonParams = DealParamsUtil.getDealParams(netOption);
-            pringLog(netOption.getUrl(),jsonParams);
+            pringLog(netOption.getUrl(), jsonParams);
             try {
                 return OkGo.<T>post(netOption.getUrl())
                         .tag(getRequestTag(netOption))
@@ -144,9 +148,9 @@ public class NetApi {
                 e.printStackTrace();
             }
 
-        }else if(requestMethod == RequestMethod.POST_FORM){
+        } else if (requestMethod == RequestMethod.POST_FORM) {
             String jsonParams = DealParamsUtil.getDealParams(netOption);
-            pringLog(netOption.getUrl(),jsonParams);
+            pringLog(netOption.getUrl(), jsonParams);
 
             HttpParams httpParams = new HttpParams();
             httpParams.put(netOption.getParams());
@@ -166,12 +170,12 @@ public class NetApi {
     /**
      * @return 设置请求的tag，用于之后的取消请求，默认为当前请求的activity或Fragment的类名
      */
-    public static String getRequestTag(NetOption netOption){
+    public static String getRequestTag(NetOption netOption) {
         //设置请求的key，用于之后的取消，默认为当前activity或fragment的类名
         String mKey = netOption.getUrl();
-        if (null!= netOption.getTag()){
+        if (null != netOption.getTag()) {
             mKey = netOption.getTag();
-        }else {
+        } else {
             if (null != netOption.getFragment()) {
                 mKey = ObjectUtils.getClassPath(netOption.getFragment());
             } else if (null != netOption.getActivity()) {
@@ -192,26 +196,27 @@ public class NetApi {
         return getFormDataRX(netOption);
     }
 
-    public static <T> Observable<T> getDataRX(NetOption netOption){
-        return getDataRX(RequestMethod.POST_JSON,netOption);
+    public static <T> Observable<T> getDataRX(NetOption netOption) {
+        return getDataRX(RequestMethod.POST_JSON, netOption);
     }
 
-    public static <T> Observable<T> getFormDataRX(NetOption netOption){
-        return getDataRX(RequestMethod.POST_FORM,netOption);
+    public static <T> Observable<T> getFormDataRX(NetOption netOption) {
+        return getDataRX(RequestMethod.POST_FORM, netOption);
     }
 
     /**
      * 网络请求封装，传泛型ApiBean类，使用rxjava形式返回observable
+     *
      * @param requestMethod 请求方式
      * @param netOption
-     * @return
      * @param <T>
+     * @return
      */
     public static <T> Observable<T> getDataRX(@RequestMethod int requestMethod, NetOption netOption) {
         if (!NetworkUtils.isConnected()) {
             return Observable.error(new NoNetException());
         }
-        if(requestMethod == RequestMethod.GET){
+        if (requestMethod == RequestMethod.GET) {
             DealParamsUtil.dealUrlForGet(netOption);
             pringLog(netOption.getUrl());
             return OkGo.<T>get(netOption.getUrl())
@@ -219,18 +224,18 @@ public class NetApi {
                     .headers(netOption.getHeaders())
                     .converter(new JsonConvert2<T>(netOption))
                     .adapt(new ObservableBody<T>());
-        }else if(requestMethod == RequestMethod.POST_JSON){
+        } else if (requestMethod == RequestMethod.POST_JSON) {
             String jsonParams = DealParamsUtil.getDealParams(netOption);
-            pringLog(netOption.getUrl(),jsonParams);
+            pringLog(netOption.getUrl(), jsonParams);
             return OkGo.<T>post(netOption.getUrl())
                     .tag(NetApi.getRequestTag(netOption))
                     .headers(netOption.getHeaders())
                     .upJson(jsonParams)
                     .converter(new JsonConvert2<T>(netOption))
                     .adapt(new ObservableBody<T>());
-        }else if(requestMethod == RequestMethod.POST_FORM){
+        } else if (requestMethod == RequestMethod.POST_FORM) {
             String jsonParams = DealParamsUtil.getDealParams(netOption);
-            pringLog(netOption.getUrl(),jsonParams);
+            pringLog(netOption.getUrl(), jsonParams);
             HttpParams httpParams = new HttpParams();
             httpParams.put(netOption.getParams());
             return OkGo.<T>post(netOption.getUrl())
@@ -249,53 +254,58 @@ public class NetApi {
     ///////////////////////////////////////////////////////////////////////////
 
     /**
-     * 上传的请求，传泛型，返回泛型的内容
-     *
-     * @param <T>  泛型
+     * 单文件上传
      */
     public static <T> void upFileData(NetOption netOption, File file, JsonCallback<T> jsonCallback) {
         if (!NetworkUtils.isConnected()) {
             jsonCallback.onNoNet();
             return;
         }
-
-        /*String jsonParams = DealParamsUtil.getDealParams(netOption);
-        if (AppConfig.isDebug) {
-            Logger.d(netOption.getUrl());
-            Logger.d(jsonParams);
-        }
-        HttpParams httpParams = new HttpParams();
-        httpParams.put(netOption.getParams());*/
-
         OkGo.<T>post(netOption.getUrl())
                 .tag(getRequestTag(netOption))
                 .headers(netOption.getHeaders())
-                .params("file",file)
+                .params("file", file)
+                .execute(jsonCallback);
+    }
+
+    /**
+     * 多文件上传
+     */
+    public static <T> void upFileListData(NetOption netOption, List<File> files, JsonCallback<T> jsonCallback) {
+        if (!NetworkUtils.isConnected()) {
+            jsonCallback.onNoNet();
+            return;
+        }
+        OkGo.<T>post(netOption.getUrl())
+                .tag(getRequestTag(netOption))
+                .isMultipart(true)
+                .headers(netOption.getHeaders())
+                .addFileParams("files", files)
                 .execute(jsonCallback);
     }
 
     /**
      * @param tag 取消某个tag的请求
      */
-    public static void cancelTag(Object tag){
+    public static void cancelTag(Object tag) {
         OkGo.getInstance().cancelTag(tag);
     }
 
     /**
      * 取消所有请求
      */
-    public static void cancelAll(){
+    public static void cancelAll() {
         OkGo.getInstance().cancelAll();
     }
 
-    private static void pringLog(String url,String jsonParams){
-        if(!AppConfig.isDebug)return;
+    private static void pringLog(String url, String jsonParams) {
+        if (!AppConfig.isDebug) return;
         Logger.t("real-request").d(url);
         Logger.t("real-request").d(jsonParams);
     }
 
-    private static void pringLog(String url){
-        if(!AppConfig.isDebug)return;
+    private static void pringLog(String url) {
+        if (!AppConfig.isDebug) return;
         Logger.t("real-request").d(url);
     }
 }
--- baselibrary/src/main/java/com/dengyun/baselibrary/net/upload/UploadBean.java
+++ baselibrary/src/main/java/com/dengyun/baselibrary/net/upload/UploadBean.java
@@ -5,13 +5,6 @@ package com.dengyun.baselibrary.net.upload;
  */
 public class UploadBean {
 
-    /**
-     * message : 成功
-     * result : 200
-     * file_url : http://resource.idengyun.com/resource/images/2016/05/05451722-bdf2-43d7-b281-68c230c685d8.png
-     * file_url0 : null
-     */
-
     public int code;
     public boolean success;
     public String msg;
